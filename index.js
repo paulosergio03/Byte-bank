b1afce8298cc6863c7d348e42bf1546a26142eda
Paulo Sergio da Silva Sousa <paulo.sousa@cegas.com.br>
2022-05-03 15:32:37 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,22 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+const cliente1Nome = "Alan";
+const cliente1CPF = 1112223309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Paulo";
+const cliente2CPF = 8882223309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+const cliente3Nome = "Kelly";
+const cliente3CPF = 9992223309;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+console.log(cliente1Nome);
